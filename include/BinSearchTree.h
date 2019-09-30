014dba3cc53556cc4d93d1f32ade96c7d31dab27
Matheus de Andrade Silva <matheus.mas132@hotmail.com>
2019-09-30 19:40:43 -0300

    bstree's methods declared

--- include/BinSearchTree.h
+++ include/BinSearchTree.h
@@ -2,6 +2,7 @@
 #define BINSEARCHTREE_H
 
 #include <iostream>
+#include <string>
 
 class BinSearchTree {
 
@@ -11,6 +12,33 @@ class BinSearchTree {
 
 		/// Destructor
 		~BinSearchTree( );
+
+		/// Search an element
+		bool search( int element );
+
+		/// Insert an element
+		bool insert( int element );
+
+		/// Remove an element
+		bool remove( int element );
+
+		/// nth_element
+		int nth_element( int n );
+
+		/// position
+		int position( int element );
+
+		/// median
+		int median( );
+
+		/// is_full
+		bool is_full( );
+
+		/// is_complete
+		bool is_complete( );
+
+		/// to_string
+		std::string to_string( );
 
 };
 
