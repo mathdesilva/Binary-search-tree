b2da7a017c6b790ef5f9e54c22af68c35e384544
Matheus de Andrade Silva <matheus.mas132@hotmail.com>
2019-10-01 07:49:47 -0300

    test code is calling functions

--- src/BinSearchTree.cpp
+++ src/BinSearchTree.cpp
@@ -3,75 +3,75 @@
 
 /// Constructor
 BinSearchTree::BinSearchTree( ) {
-	std::cout << "BST Constructor\n";
+	// std::cout << "test :: BST Constructor\n";
 	// TODO
 }
 
 /// Destructor
 BinSearchTree::~BinSearchTree( ) {
-	std::cout << "BST Destructor\n";
+	// std::cout << "test :: BST Destructor\n";
 	// TODO
 }
 
 /// Search an element
 bool BinSearchTree::search( int element ) {
 	// TODO
-	std::cout << "test :: seach method - " << element << std::endl;
+	// std::cout << "test :: seach method - " << element << std::endl;
 	return true;
 }
 
 /// Insert an element
 bool BinSearchTree::insert( int element ) {
 	// TODO
-	std::cout << "test :: insert method - " << element << std::endl;
+	// std::cout << "test :: insert method - " << element << std::endl;
 	return true;
 }
 
 /// Remove an element
 bool BinSearchTree::remove( int element ) {
 	// TODO
-	std::cout << "test :: remove method - " << element << std::endl;
+	// std::cout << "test :: remove method - " << element << std::endl;
 	return true;
 }
 
 /// nth_element
 int BinSearchTree::nth_element( int n ) {
 	// TODO
-	std::cout << "test :: nth_element method - " << n << std::endl;
+	// std::cout << "test :: nth_element method - " << n << std::endl;
 	return 10;
 }
 
 /// position
 int BinSearchTree::position( int element ) {
 	// TODO
-	std::cout << "test :: position method - " << element << std::endl;
+	// std::cout << "test :: position method - " << element << std::endl;
 	return 10;
 }
 
 /// median
 int BinSearchTree::median( ) {
 	// TODO
-	std::cout << "test :: median method" << std::endl;
+	// std::cout << "test :: median method" << std::endl;
 	return 10;
 }
 
 /// is_full
 bool BinSearchTree::is_full( ) {
 	// TODO
-	std::cout << "test :: is_full method" << std::endl;
+	// std::cout << "test :: is_full method" << std::endl;
 	return true;
 }
 
 /// is_complete
 bool BinSearchTree::is_complete( ) {
 	// TODO
-	std::cout << "test :: is_complete method" << std::endl;
+	// std::cout << "test :: is_complete method" << std::endl;
 	return true;
 }
 
 /// to_string
 std::string BinSearchTree::to_string( ) {
 	// TODO
-	std::cout << "test :: to_string method" << std::endl;
+	// std::cout << "test :: to_string method" << std::endl;
 	return "abcde";
 }
